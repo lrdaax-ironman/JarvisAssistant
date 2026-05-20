dcbc0498af2a1bfded41d9173fcfb5af3375672e
LRDAX <wyllan11@gmail.com>
2026-05-20 07:14:29 +0200

    Expose secure desktop bridge

--- preload.js
+++ preload.js
@@ -1,4 +1,6 @@
-const { contextBridge } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
+
+const allowedFolders = ["documents", "desktop"];
 
 // Pont minimal entre Electron et l'interface, sans exposer Node.js.
 contextBridge.exposeInMainWorld("jarvisDesktop", {
@@ -7,5 +9,18 @@ contextBridge.exposeInMainWorld("jarvisDesktop", {
   versions: {
     chrome: process.versions.chrome,
     electron: process.versions.electron
-  }
+  },
+  openCalculator: () => ipcRenderer.invoke("jarvis:open-calculator"),
+  openBrowser: () => ipcRenderer.invoke("jarvis:open-browser"),
+  openFolder: (folderKey) => {
+    if (!allowedFolders.includes(folderKey)) {
+      return Promise.resolve({ ok: false, message: "Dossier non autorise." });
+    }
+
+    return ipcRenderer.invoke("jarvis:open-folder", folderKey);
+  },
+  getSystemInfo: () => ipcRenderer.invoke("jarvis:get-system-info"),
+  setFullScreen: (enabled) => ipcRenderer.invoke("jarvis:set-fullscreen", Boolean(enabled)),
+  minimize: () => ipcRenderer.invoke("jarvis:minimize"),
+  closeApp: () => ipcRenderer.invoke("jarvis:close-app")
 });
